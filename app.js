d65e73db16b1861bd50e67d12056aa1e689a8564
Nilar <nilar20633@gmail.com>
2025-11-13 00:42:53 +0700

    Part-4 Update localStorage and refresh the UI to show the active and checked task states

--- app.js
+++ app.js
@@ -4,14 +4,31 @@ let todosList = document.querySelector(".todos-list");
 // datas from local storage
 let todos = JSON.parse(localStorage.getItem("todo-list"));
 
+// update status on local storage and UI
+function statusUpdate(item) {
+  let task = item.parentElement.lastElementChild;
+  if (item.checked) {
+    task.classList.add("active");
+    // update local storage
+    todos[item.id].status = "complete";
+  } else {
+    task.classList.remove("active");
+    todos[item.id].status = "pending";
+  }
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+}
+
 // to show todos task from local storage
 function showTodos() {
   todos.forEach((tasks, id) => {
+    // update completeStatus and completeTaskCheck in UI
+    let completeStatus = tasks.status === "complete" ? "active" : "";
+    let completeTaskCheck = tasks.status === "complete" ? "checked" : "";
     todosList.innerHTML += `
     <li class="todo-items">
       <label for="${id}">
-        <input type="checkbox" id="${id}" />
-        <span class="todo-txt">${tasks.task}</span>
+        <input type="checkbox" id="${id}" onclick="statusUpdate(this)" ${completeTaskCheck}/>
+        <p class="todo-txt ${completeStatus}" >${tasks.task}</p>
       </label>
       <div>
         <i class="fa-solid fa-pen-to-square edit-ic"></i>
@@ -20,6 +37,7 @@ function showTodos() {
     </li>`;
   });
 }
+showTodos();
 
 userTaskInput.addEventListener("keyup", (e) => {
   let userInput = userTaskInput.value.trim();
@@ -32,6 +50,5 @@ userTaskInput.addEventListener("keyup", (e) => {
     let userTasks = { task: userInput, status: "pending" };
     todos.push(userTasks);
     localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodos();
   }
 });
